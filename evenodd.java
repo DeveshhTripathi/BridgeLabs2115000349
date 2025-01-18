627f85efbf699ca24cc54b4993828c15e87d7887
DeveshhTripathi <deveshht591@gmail.com>
2025-01-19 00:44:33 +0530

    This is commit for first code

---
+++ evenodd.java
@@ -0,0 +1,12 @@
+public class evenodd{
+	public static void main(String Args[]){
+	for(int i=0;i<=10;i++){
+     if(i%2==0){
+                System.out.println(i +"is Even");
+      }
+        else{
+        System.out.println(i + "is odd");
+	}
+}
+}
+}
